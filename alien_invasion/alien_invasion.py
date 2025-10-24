155bd6a0c252c3244f08518e15770e925deeb5c9
diego-tomasso <diego.tomasso@gmail.com>
2025-10-24 20:02:24 -0300

    Adding key event to quit the game and (commented) the needed code to run the game fullscreen mode, which I don't like it.

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -14,6 +14,12 @@ class AlienInvasion:
 
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
+        
+        # FULLSCREEN:
+        # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
+        
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
@@ -42,6 +48,8 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
     
     def _check_keyup_events(self, event):
         """Respond to keypresses."""
